725a69da42dde1c8e5a6c6158d80a6c49e1e6123
Christoph Papke <christoph.papke@kaba.com>
2017-11-20 16:36:13 +0100

    Increase session timeout for mongo http sessions

--- modules/cloud-portal-server/src/main/java/de/papke/cloud/portal/auth/HttpSessionConfig.java
+++ modules/cloud-portal-server/src/main/java/de/papke/cloud/portal/auth/HttpSessionConfig.java
@@ -4,11 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.session.data.mongo.JdkMongoSessionConverter;
 import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
 
-@EnableMongoHttpSession
+@EnableMongoHttpSession(maxInactiveIntervalInSeconds = HttpSessionConfig.SESSION_TIMEOUT)
 public class HttpSessionConfig {
 	
+	public static final int SESSION_TIMEOUT = 28800;
+	
 	@Bean
-    public JdkMongoSessionConverter jdkMongoSessionConverter() {
-            return new JdkMongoSessionConverter(); 
-    }
+	public JdkMongoSessionConverter jdkMongoSessionConverter() {
+		return new JdkMongoSessionConverter(); 
+	}
 }
