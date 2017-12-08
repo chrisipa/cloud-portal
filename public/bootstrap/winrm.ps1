a7df99ba951882cab95eb17b7e427e63dfbc566b
Christoph Papke <christoph.papke@kaba.com>
2017-12-08 16:00:03 +0100

    Powershell script for configuring windows remoting and firewall rules

--- public/bootstrap/winrm.ps1
+++
@@ -1,2 +0,0 @@
-winrm create winrm/config/listener?Address=*+Transport=HTTP 
-netsh advfirewall firewall add rule name="WinRM in" protocol=TCP dir=in profile=any localport=5985 remoteip=any localip=any action=allow
