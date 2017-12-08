2b831053271c44a5444c914cffab20f679dac2c2
Christoph Papke <christoph.papke@kaba.com>
2017-12-08 15:11:01 +0100

    Powershell script for configuring windows remoting and firewall rules

--- public/bootstrap/winrm.ps1
+++ public/bootstrap/winrm.ps1
@@ -1,2 +1,5 @@
-winrm quickconfig -q & winrm set winrm/config @{MaxTimeoutms="1800000"} & winrm set winrm/config/service @{AllowUnencrypted="true"} & winrm set winrm/config/service/auth @{Basic="true"}
+winrm quickconfig -q 
+winrm set winrm/config @{MaxTimeoutms="1800000"}
+winrm set winrm/config/service @{AllowUnencrypted="true"}
+winrm set winrm/config/service/auth @{Basic="true"}
 netsh advfirewall firewall add rule name="WinRM in" protocol=TCP dir=in profile=any localport=5985 remoteip=any localip=any action=allow
