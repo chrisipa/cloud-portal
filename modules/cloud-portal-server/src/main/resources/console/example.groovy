dc0d4ecc66d5c1a0e72d39950c759f1e822b6b3f
Christoph Papke <christoph.papke@kaba.com>
2018-07-03 09:49:43 +0200

    Added scripting console example for accesing the mongo db database

--- modules/cloud-portal-server/src/main/resources/console/example.groovy
+++ modules/cloud-portal-server/src/main/resources/console/example.groovy
@@ -1,2 +1,9 @@
-// get user
-userService.getUser("my-mail-account@my-domain.com");
+// 1. Example: Find one provision log entry in mongo db
+// import com.mongodb.*
+// mongoClient = new MongoClient("mongodb" , 27017);
+// db = mongoClient.getDB( "cloud-portal" );
+// collection = db.getCollection("provisionLog");
+// collection.findOne();
+
+// 2. Example: Get user from userService
+// userService.getUser("my-mail-account@my-domain.com");
