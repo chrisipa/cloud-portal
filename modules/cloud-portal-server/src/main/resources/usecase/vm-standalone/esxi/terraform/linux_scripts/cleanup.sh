d835f21ef1860e9e92f80eb4c785a57349105145
Christoph Papke <chrisipa@users.noreply.github.com>
2021-05-24 12:49:55 +0200

    Added compatibility with Ubuntu Server 20.04 LTS

--- modules/cloud-portal-server/src/main/resources/usecase/vm-standalone/esxi/terraform/linux_scripts/cleanup.sh
+++ modules/cloud-portal-server/src/main/resources/usecase/vm-standalone/esxi/terraform/linux_scripts/cleanup.sh
@@ -9,7 +9,7 @@ diskPath="$(sudo pvdisplay | grep 'PV Name' | awk '{print $3}')"
 
 # get lvm path
 lvmName="$(sudo pvdisplay | grep 'VG Name' | awk '{print $3}')"
-lvmPath="/dev/$lvmName/root"
+lvmPath="$(find /dev/$lvmName -type l | head -n 1)"
 
 # resize lvm to 100% disk space
 sudo "$scriptFolder/lvm-resize.sh" -p "$diskPath" -l "$lvmPath" -f
